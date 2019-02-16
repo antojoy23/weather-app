fe96967486e64bc3bc09045e929909bef54b2298
Anto Joy <antojoy23@gmail.com>
2019-02-16 13:13:36 +0530

    Updates

---
+++ precache-manifest.865ede7f4a5385eb16fda2da687dc215.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b20b419f145729429b35",
+    "url": "/antojoy23/weather-app/static/js/runtime~main.b20b419f.js"
+  },
+  {
+    "revision": "7876a721d31a5a99bddd",
+    "url": "/antojoy23/weather-app/static/js/main.7876a721.chunk.js"
+  },
+  {
+    "revision": "b33b4f278e674be82cad",
+    "url": "/antojoy23/weather-app/static/js/2.b33b4f27.chunk.js"
+  },
+  {
+    "revision": "7876a721d31a5a99bddd",
+    "url": "/antojoy23/weather-app/static/css/main.973d281f.chunk.css"
+  },
+  {
+    "revision": "d5bad9aa7109367ad1e0f80dd7fde21e",
+    "url": "/antojoy23/weather-app/index.html"
+  }
+];
